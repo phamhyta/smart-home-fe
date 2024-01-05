480a846d5b30345801247fb76bc57eeae14be91a
huongtx <phamhyta@gmail.com>
2024-01-05 23:53:35 +0700

    edit url be

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,7 +4,8 @@ import axios from "axios";
 const sTag = "[AxiosClient]";
 
 const axiosClient = axios.create({
-  baseURL: "http://172.20.10.2:8080/api/",
+  // baseURL: "http://172.20.10.2:8080/api/",
+  baseURL:"http://localhost:8080/api/",
   headers: {
     "Content-Type": "application/json",
   },
