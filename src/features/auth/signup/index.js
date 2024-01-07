d0d7c1d521f5c73833c78721826b20bb41a653f4
huongtx <phamhyta@gmail.com>
2024-01-07 10:57:55 +0700

    fix login signin layout

--- src/features/auth/signup/index.js
+++ src/features/auth/signup/index.js
@@ -4,7 +4,8 @@ import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from "react-toastify";
 import authApi from '../../../api/authApi';
-import './style.scss'
+import bgImg from '../../../assets/bg-login-2.jpg'
+import '../login/style.scss';
 
 function Signup () {
     const navigate = useNavigate()
@@ -30,30 +31,36 @@ function Signup () {
 
     return (
         <div className="login-screen d-flex flex-column">
-            <div style={{background: '#FFFFFF', borderRadius: '30px', width: '40%'}} className='p-5 text-center'>
-                <p className='title'>SIGNUP</p>
-                <Form className='login-form text-start'>
-                    <Form.Group>
-                        <Form.Label>Fullname</Form.Label>
-                        <Form.Control type='text' name="fullname" onChange={(e) => handleChange(e)}/>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type='text' name="username" onChange={(e) => handleChange(e)}/>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type='password' name="password" onChange={(e) => handleChange(e)}/>
-                    </Form.Group>
-                    {/* <Form.Group>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type='password' />
-                    </Form.Group> */}
-                </Form>
-                <p className='text-start'>Already have an account? <Link to='/login'>Login</Link></p>
-                <Button className='btn-login mt-4' onClick={handleSignup}>Signup</Button>
-            </div>
-            
+            <div className='d-flex w-100 justify-content-center mt-5'>
+                <div style={{width: '30%'}}>
+                    <img className='w-100 h-100' src={bgImg} alt="" />
+                </div>
+                <div style={{width: '30%'}} className='bg-white py-2 px-4 text-center'>
+                    <p className='title'>SIGNUP</p>
+                    <Form className='login-form text-start'>
+                        <Form.Group>
+                            <Form.Label>Fullname</Form.Label>
+                            <Form.Control type='text' name="fullname" onChange={(e) => handleChange(e)}/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type='text' name="username" onChange={(e) => handleChange(e)}/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type='password' name="password" onChange={(e) => handleChange(e)}/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Password confirm</Form.Label>
+                            <Form.Control type='password' name="passwordConfirm"/>
+                        </Form.Group>
+                    </Form>
+                    <p className='text-start mt-2'>Already have an account? <Link to='/login'>Login</Link></p>
+                    <div>
+                        <Button className='btn-login mt-2' onClick={handleSignup}>Signup</Button>
+                    </div>
+                </div>
+           </div>
         </div>
     )
 }
